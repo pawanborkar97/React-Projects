03a08a7cf9a1fd946b0a625a3205def8bd8170a5
pawan@BOOK-S64AV9OJD6./OneDrive/Desktop/React-Projects <pawanborkakmail@gmail.com>
2025-07-13 21:24:11 +0530

    React Started

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const Header = () => {
     return (
         <div className="header">   
             <div className="logo-container">
-            <img src="https://cdn.vectorstock.com/i/500p/23/62/modern-organic-food-logo-and-natural-vector-53742362.jpg" />
-            
+            <img className="logo" src="https://cdn.vectorstock.com/i/500p/23/62/modern-organic-food-logo-and-natural-vector-53742362.jpg" ></img>
+            </div>
              <div className="nav-items"> 
                 <ul> 
                 <li>Home</li>
@@ -14,16 +17,825 @@ const Header = () => {
 
              </div>
             </div>  
-        </div>
     )
 } 
     
+const Restaurantcard = (props) => {
+    const {resData} = props;
+    const {cloudinaryImageId, name, cuisines, avgRating,deliveryTime} = resData?.info;
+    return (
+        <div className="res-card" style={{ backgroundColor: "#f0f0f0"}}>
+        <img className="res-logo" alt="res-logo" 
+         src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+               +cloudinaryImageId}/>
+        <h3>{name}</h3>
+        <h4>{cuisines.join(", ")}</h4>
+        <h4>{avgRating}</h4>
+        <h4>{deliveryTime}</h4>
+        </div>
+    )
+}
+
+
+const reslist = [
+{
+"info": {
+"id": "55054",
+"name": "SanToshi's Restaurant",
+"cloudinaryImageId": "fogfnatzbqcn5nytgb7e",
+"locality": "Dharampeth",
+"areaName": "Dharampeth",
+"costForTwo": "₹300 for two",
+"cuisines": [
+"South Indian", "Snacks", "Desserts", "Beverages", "Sweets"],
+"avgRating": 4.7,
+"parentId": "168579",
+"avgRatingString": "4.7",
+"totalRatingsString": "23K+",
+"sla": {
+"deliveryTime": 24,
+"lastMileTravel": 4.8,
+"serviceability": "SERVICEABLE",
+"slaString": "20-25 mins",
+"lastMileTravelString": "4.8 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-13 22:30:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "Rxawards/_CATEGORY-South%20Indian.png",
+"description": "Delivery!"
+}
+]
+},
+"isOpen": true,
+"aggregatedDiscountInfoV2": {},
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Rxawards/_CATEGORY-South%20Indian.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "4.2",
+"ratingCount": "6.9K+"
+},
+"source": "GOOGLE",
+"sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-a8a52e3b-ecb8-4c7a-b861-7d9bb56628a5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nagpur/reddys-gokul-brindavan-restaurant-dharampeth-rest55054",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "678820",
+"name": "Sameeksha's Kitchen",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/23/6a675f98-f8aa-4dc9-b452-82a474b27065_678820.JPG",
+"locality": "Poonam Chamber Mall",
+"areaName": "Byramji Town",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Desserts"
+],
+"avgRating": 4.5,
+"parentId": "1040",
+"avgRatingString": "4.5",
+"totalRatingsString": "1.2K+",
+"sla": {
+"deliveryTime": 26,
+"lastMileTravel": 5,
+"serviceability": "SERVICEABLE",
+"slaString": "25-30 mins",
+"lastMileTravelString": "5.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-14 01:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "Rxawards/_CATEGORY-Desserts.png",
+"description": "Delivery!"
+},
+{
+"imageId": "newg.png",
+"description": "Gourmet"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Rxawards/_CATEGORY-Desserts.png"
+}
+},
+{
+"attributes": {
+"description": "Gourmet",
+"imageId": "newg.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹49"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-a8a52e3b-ecb8-4c7a-b861-7d9bb56628a5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nagpur/theobroma-poonam-chamber-mall-byramji-town-rest678820",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "553826",
+"name": "Tejaswini Coffee",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/3/17/96e838d5-2a67-488d-8722-750e6077132e_553826.JPG",
+"locality": "Dharampeth",
+"areaName": "Dharampeth",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Beverages",
+"Cafe",
+"Snacks",
+"Desserts",
+"Bakery",
+"Ice Cream"
+],
+"avgRating": 4.6,
+"parentId": "195515",
+"avgRatingString": "4.6",
+"totalRatingsString": "1.3K+",
+"sla": {
+"deliveryTime": 20,
+"lastMileTravel": 4.5,
+"serviceability": "SERVICEABLE",
+"slaString": "15-20 mins",
+"lastMileTravelString": "4.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-13 23:59:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "Green%20Dot%20Awards/Best%20In%20Veg%20Cafe.png",
+"description": "Delivery!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Green%20Dot%20Awards/Best%20In%20Veg%20Cafe.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "20% OFF",
+"discountTag": "FLAT DEAL"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "4.3",
+"ratingCount": "1.7K+"
+},
+"source": "GOOGLE",
+"sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-a8a52e3b-ecb8-4c7a-b861-7d9bb56628a5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nagpur/starbucks-coffee-dharampeth-rest553826",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "76405",
+"name": "Cafe Coffee Day",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/19/6fa4753b-2aaf-4f73-8db8-2605f7dbedc7_76405.JPG",
+"locality": "Futala Road",
+"areaName": "Civil Lines",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Beverages",
+"Cafe",
+"Snacks",
+"Desserts",
+"Burgers",
+"Ice Cream",
+"Bakery",
+"Fast Food"
+],
+"avgRating": 4.6,
+"parentId": "1",
+"avgRatingString": "4.6",
+"totalRatingsString": "1.1K+",
+"sla": {
+"deliveryTime": 21,
+"lastMileTravel": 3.7,
+"serviceability": "SERVICEABLE",
+"slaString": "20-25 mins",
+"lastMileTravelString": "3.7 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-13 23:45:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹99"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-a8a52e3b-ecb8-4c7a-b861-7d9bb56628a5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nagpur/cafe-coffee-day-futala-road-civil-lines-rest76405",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "53484",
+"name": "Haldiram's Restaurant",
+"cloudinaryImageId": "1abb89624b491497c8cff182d95ac457",
+"locality": "Sitabuldi",
+"areaName": "Sitabuldi",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"South Indian",
+"Biryani",
+"Chaat",
+"Thalis",
+"Bakery",
+"Sweets",
+"Desserts"
+],
+"avgRating": 4.5,
+"veg": true,
+"parentId": "351771",
+"avgRatingString": "4.5",
+"totalRatingsString": "13K+",
+"sla": {
+"deliveryTime": 20,
+"lastMileTravel": 2.5,
+"serviceability": "SERVICEABLE",
+"slaString": "15-20 mins",
+"lastMileTravelString": "2.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-13 23:30:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "android/static-assets/icons/big_rx.png",
+"description": "bolt!"
+},
+{
+"imageId": "Rxawards/_CATEGORY-Thali.png",
+"description": "Delivery!"
+},
+{
+"imageId": "v1690360529/Ratnesh_Badges/Only_on_swiggy_badge_4x.png",
+"description": "OnlyOnSwiggy"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "bolt!",
+"imageId": "android/static-assets/icons/big_rx.png"
+}
+},
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Rxawards/_CATEGORY-Thali.png"
+}
+},
+{
+"attributes": {
+"description": "OnlyOnSwiggy",
+"imageId": "v1690360529/Ratnesh_Badges/Only_on_swiggy_badge_4x.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹49"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "4.2",
+"ratingCount": "17K+"
+},
+"source": "GOOGLE",
+"sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-a8a52e3b-ecb8-4c7a-b861-7d9bb56628a5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nagpur/haldirams-restaurant-sitabuldi-rest53484",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "740318",
+"name": "CakeZone Patisserie SSD",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/2db6f24f-8645-46e6-8816-cd817633faf1_740318.jpg",
+"locality": "Avenue road",
+"areaName": "Nandanvan",
+"costForTwo": "₹200 for two",
+"cuisines": [
+"Bakery",
+"Desserts",
+"Ice Cream",
+"Sweets"
+],
+"avgRating": 4.4,
+"parentId": "7003",
+"avgRatingString": "4.4",
+"totalRatingsString": "714",
+"sla": {
+"deliveryTime": 41,
+"lastMileTravel": 2.4,
+"serviceability": "SERVICEABLE",
+"slaString": "40-45 mins",
+"lastMileTravelString": "2.4 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-14 03:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "android/static-assets/icons/big_rx.png",
+"description": "bolt!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "bolt!",
+"imageId": "android/static-assets/icons/big_rx.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹69"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-a8a52e3b-ecb8-4c7a-b861-7d9bb56628a5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nagpur/cakezone-patisserie-avenue-road-nandanvan-rest740318",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "510859",
+"name": "Kwality Walls Ice Cream and Aloo",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/13/eb7f4b65-43b8-47a2-af88-6350fbe9fcd2_510859.JPG",
+"locality": "Surendra Nagar",
+"areaName": "Manish Nagar",
+"costForTwo": "₹200 for two",
+"cuisines": [
+"Desserts",
+"Ice Cream",
+"Ice Cream Cakes"
+],
+"avgRating": 4.7,
+"veg": true,
+"parentId": "582",
+"avgRatingString": "4.7",
+"totalRatingsString": "1.7K+",
+"sla": {
+"deliveryTime": 27,
+"lastMileTravel": 4.9,
+"serviceability": "SERVICEABLE",
+"slaString": "25-30 mins",
+"lastMileTravelString": "4.9 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-13 23:59:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"aggregatedDiscountInfoV2": {},
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-a8a52e3b-ecb8-4c7a-b861-7d9bb56628a5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nagpur/kwality-walls-ice-cream-and-more-surendra-nagar-manish-nagar-rest510859",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "741991",
+"name": "Cheesecakes By CakeZone",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/5/15/aca6431c-f3a8-414d-8175-799fdb32c918_741991.jpg",
+"locality": "Roke Sadan",
+"areaName": "Nandanvan",
+"costForTwo": "₹200 for two",
+"cuisines": [
+"Desserts",
+"Ice Cream",
+"Juices",
+"Bengali"
+],
+"avgRating": 3.9,
+"veg": true,
+"parentId": "348057",
+"avgRatingString": "3.9",
+"totalRatingsString": "94",
+"sla": {
+"deliveryTime": 30,
+"lastMileTravel": 2.4,
+"serviceability": "SERVICEABLE",
+"slaString": "25-30 mins",
+"lastMileTravelString": "2.4 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-14 03:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "newg.png",
+"description": "Gourmet"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Gourmet",
+"imageId": "newg.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹69"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-a8a52e3b-ecb8-4c7a-b861-7d9bb56628a5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nagpur/cheesecakes-by-cakezone-roke-sadan-nandanvan-rest741991",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "53534",
+"name": "Airport Centre Point - Wardha Road - CHANDA",
+"cloudinaryImageId": "614cef1b8b369f8a370d749be049a174",
+"locality": "Wardha Road",
+"areaName": "Wardha Road",
+"costForTwo": "₹300 for two",
+"cuisines": [
+"Biryani",
+"Chinese",
+"Tandoor",
+"Beverages"
+],
+"avgRating": 4.5,
+"parentId": "618436",
+"avgRatingString": "4.5",
+"totalRatingsString": "10K+",
+"sla": {
+"deliveryTime": 35,
+"lastMileTravel": 7,
+"serviceability": "SERVICEABLE",
+"slaString": "30-35 mins",
+"lastMileTravelString": "7.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-07-14 05:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "newg.png",
+"description": "Gourmet"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Gourmet",
+"imageId": "newg.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "₹100 OFF",
+"subHeader": "ABOVE ₹299",
+"discountTag": "FLAT DEAL"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "3.9",
+"ratingCount": "135"
+},
+"source": "GOOGLE",
+"sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-a8a52e3b-ecb8-4c7a-b861-7d9bb56628a5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nagpur/airport-centre-point-wardha-road-wardha-road-rest53534",
+"type": "WEBLINK"
+}
+}
+];
+
+const Body = () => {
+    return (
+        <div className="body">
+        <div className="search"> Search </div>
+        <div className="res-container">
+            {
+               reslist.map ((restaurant, index) =>
+               (<Restaurantcard key={restaurant.info.id}resData = {restaurant}/>
+
+               ))
+            }
+        </div>
+        </div>
+    )
+}
     const AppLayout = () => {
     return ( 
             <div className="app">
                 <Header/>
+                <Body/>
             </div>  
-               )
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -33,26 +845,15 @@ root.render(<AppLayout/>);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 /*
 *<div id="parent">
     <div id="child1"> 
-        <h1> h1 tag </h1>
-        <h2> h2 tag </h2>
+        <h1> h1 tag Toshi 1 </h1>
+        <h2> h2 tag Tejaswini 1 </h2>
     </div>,
     <div id="child2">
-        <h1> h3 tag </h1>
-        <h2> h4 tag </h2>
+        <h1> h3 tag Sam 1 </h1>
+        <h2> h4 tag Samsung 1 </h2>
     </div>
 *</div>
 React Element (Object) = HTML Element (Browser Understand)
